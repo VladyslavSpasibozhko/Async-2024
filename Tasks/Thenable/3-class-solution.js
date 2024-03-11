6f7c66647770b81b6c894a37ee2db9eb434ff577
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2024-03-11 22:01:35 +0200

    Add Thenable tasks

---
+++ Tasks/Thenable/3-class-solution.js
@@ -0,0 +1,35 @@
+'use strict';
+
+class Iterator {
+  #index = 0;
+  #items = null;
+
+  constructor(array) {
+    this.#items = array;
+  }
+
+  then(fulfill, reject) {
+    if (this.#index < this.#items.length) {
+      return void fulfill(this.#items[this.#index++]);
+    }
+    reject(new Error('No more items to iterate'));
+  }
+}
+
+const electronics = [
+  { name: 'Laptop', price: 1500 },
+  { name: 'Keyboard', price: 100 },
+  { name: 'HDMI cable', price: 10 },
+];
+
+(async () => {
+  const items = new Iterator(electronics);
+  const item1 = await items;
+  console.log(item1);
+  const item2 = await items;
+  console.log(item2);
+  const item3 = await items;
+  console.log(item3);
+  const item4 = await items;
+  console.log(item4);
+})();
