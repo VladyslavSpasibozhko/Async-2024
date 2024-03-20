87be88753bc26e54072eeb588a2bd58ea320db50
Vladyslav Spasibozhko <vladspasibozhko@gmail.com>
2024-03-20 21:25:56 +0100

    complete async await block

--- Tasks/AsyncAwait/3-constructor-problem.js
+++ Tasks/AsyncAwait/3-constructor-problem.js
@@ -1,38 +1,39 @@
-'use strict';
+"use strict";
 
 // Task: rewrite class Basket to class Total with async constructor
 // Constructor call example:
 //   const total = await new Total(electronics);
 //   console.log({ total });
 
-class Basket {
-  #items = null;
-
+class Total {
   constructor(items) {
-    this.#items = items;
+    return this.#total(items);
   }
 
-  total(callback) {
+  async #total(items) {
     let result = 0;
-    for (const item of this.#items) {
+    for (const item of items) {
       if (item.price < 0) {
-        callback(new Error('Negative price is not allowed'));
-        return;
+        new Error("Negative price is not allowed");
       }
       result += item.price;
     }
-    callback(null, result);
+
+    return result;
   }
 }
 
 const electronics = [
-  { name: 'Laptop', price: 1500 },
-  { name: 'Keyboard', price: 100 },
-  { name: 'HDMI cable', price: 10 },
+  { name: "Laptop", price: 1500 },
+  { name: "Keyboard", price: 100 },
+  { name: "HDMI cable", price: 10 },
 ];
 
-const basket = new Basket(electronics);
-basket.total((error, money) => {
-  if (error) console.error({ error });
-  else console.log({ money });
-});
+(async () => {
+  try {
+    const total = await new Total(electronics);
+    console.log({ total });
+  } catch (err) {
+    console.log(err.message);
+  }
+})();
